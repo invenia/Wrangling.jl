2cb0ffb134f960ff8c360e255ceb11c97fab5ca9
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-03-06 16:28:05 +0000

    docs build

--- docs/make.jl
+++ docs/make.jl
@@ -5,19 +5,20 @@ makedocs(;
     format=Documenter.HTML(),
     pages=[
         "Home" => "index.md",
-        "Working with Symbol" => "symbol_searching.md",
+        "Working with Symbols" => "symbol_searching.md",
         "API" => "api.md",
     ],
     repo="https://github.com/invenia/Wrangling.jl/blob/{commit}{path}#L{line}",
     sitename="Wrangling.jl",
     authors="Invenia Technical Computing Corporation",
-    assets=[
+    #== assets=[
         "assets/invenia.css",
         "assets/logo.png",
-    ],
-    #strict=true,
+    ],==#
+    strict=true,
 )
 
 deploydocs(;
     repo="github.com/invenia/Wrangling.jl",
+    push_preview=true,
 )
