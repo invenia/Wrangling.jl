494906f5b3b2c6e49503b215cb4d8d6b229a6ac5
Nick Robinson <nicholas.robinson@invenialabs.co.uk>
2020-05-27 17:53:14 +0100

    Avoid name conflict with `Base.contains` in Julia 1.5

--- src/symbol_searching.jl
+++ src/symbol_searching.jl
@@ -1,17 +1,25 @@
+# `contains` was added to Julia Base in 1.5.0-DEV.639
+# https://github.com/JuliaLang/julia/commit/cc6e121386758dff6ba7911770e48dfd59520199
+@static if VERSION <= v"1.5.0-DEV.639"
+    """
+        contains(haystack, needle)
 
-"""
-    contains(haystack, needle)
-
-This is the same as `Base.occurin(needle, haystack)`, just with the arguments reversed.
-This makes it line up with `startswith` and `endswith`.
-"""
-contains(haystack, needle) = occursin(needle, haystack)
+    This is the same as `Base.occurin(needle, haystack)`, just with the arguments reversed.
+    This makes it line up with `startswith` and `endswith`.
+    """
+    contains(haystack, needle) = occursin(needle, haystack)
+end
 
 for (mod, f) in ((Base, :startswith), (Base, :endswith), (Wrangling, :contains))
     # add Symbol overloads
     @eval $mod.$f(haystack::Symbol, needle::Symbol) = $f(String(haystack), String(needle))
-    @eval $mod.$f(haystack, needle::Symbol) = $f(haystack, String(needle))
     @eval $mod.$f(haystack::Symbol, needle) = $f(String(haystack), needle)
+    if f === :contains
+        # `AbstractString` constraint needed to avoid ambiguities with Base in v1.5+.
+        @eval $mod.$f(haystack::AbstractString, needle::Symbol) = $f(haystack, String(needle))
+    else
+        @eval $mod.$f(haystack, needle::Symbol) = $f(haystack, String(needle))
+    end
 
     # add _any varients
     @eval begin
